ed8586da9acae4464afe2b32c3b7252d0a1d90ff
mai <146790723+andoMai@users.noreply.github.com>
2025-06-06 04:08:55 +0000

    liffIdの修正

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,16 @@ function App() {
   const [name, setName] = useState("");
 
   useEffect(() => {
-    // liff
-    //   .init({
-    //     liffId: import.meta.env.VITE_LIFF_ID
-    //   })
-    //   .then(() => {
-    //     liff.getProfile()
-    //       .then((profile) => {
-    //         setName(profile.displayName);
-    //       })
-    //   })
-    setName("LINE ミニアプリ");
+    liff
+      .init({
+        liffId: import.meta.env.VITE_LIFF_ID
+      })
+      .then(() => {
+        liff.getProfile()
+          .then((profile) => {
+            setName(profile.displayName);
+          })
+      })
   }, []);
   
   return (
