1624bb326b69d99f91fe9cf21dda1af927cdc1c2
mai <146790723+andoMai@users.noreply.github.com>
2025-06-06 05:03:44 +0000

    MUI追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import liff from "@line/liff";
 import "./App.css";
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 function App() {
   const [name, setName] = useState("");
@@ -20,8 +23,17 @@ function App() {
   
   return (
     <div className="App">
-      {name && <p>こんにちは、{name}さん</p>}
-    </div>
+      <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+          Material UI Preact example
+          {name && <p>こんにちは、{name}さん</p>}
+        </Typography>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  </div>
   );
 }
 
